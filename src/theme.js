ef259c1f5baa6b2638018c319d8c4dc0d9ca33ec
hatikooo <hatikooo@tutamail.com>
2024-03-29 16:19:08 +0200

    Improve database queries

---
+++ src/theme.js
@@ -0,0 +1,12 @@
+const defaultTheme = {
+  theme: {
+    headerHeight: "84px",
+    footerHeight: "31px",
+    menuSlideoutWidth: "322px",
+    sideBySideLayoutBreakpoint: "900px",
+    textColorLight: "grey",
+    textColorDark: "#4d4d4d",
+  },
+};
+
+export const reserve = defaultTheme;
